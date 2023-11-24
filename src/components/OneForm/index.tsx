74d0a32a48bd9e9c616088b82967cea59ab00a85
Levan Katsadze <leodevbro@gmail.com>
2023-11-24 21:24:48 +0400

    step next 003

--- src/components/OneForm/index.tsx
+++ src/components/OneForm/index.tsx
@@ -1,10 +1,78 @@
 'use client';
+import { OneQuestion } from '@/components/OneForm/OneQuestion';
+import { useEffect, useMemo, useState } from 'react';
 import styled from 'styled-components';
+
+import { MainParamsOfUrl, useEasyUrlQuery } from '@/customHooks/main';
+import { allForms } from '@/feed';
 
 const Ground = styled.div`
   border: 1px solid blue;
 `;
 
-export const OneForm = () => {
-  return <Ground>sdfsdf</Ground>;
+const QuestionsList = styled.div`
+  border: 1px solid green;
+`;
+
+type OneFormProps = {
+  formFeed: nsForm.One;
+};
+
+export const OneForm = ({ formFeed }: OneFormProps) => {
+  const { urlQueryParams, updateUrlQueryParams } =
+    useEasyUrlQuery<MainParamsOfUrl>();
+
+  const currForm = useMemo(() => {
+    // const foundForm = allForms.n0
+  }, [urlQueryParams]);
+
+  console.log('urlQueryParams:::', urlQueryParams);
+
+  const [fillingOfTheForm, setFillingOfTheForm] = useState<nsForm.One>(
+    structuredClone(formFeed),
+  );
+
+  const currFormLang = urlQueryParams.formLang;
+
+  useEffect(() => {
+    // if formFeed changes, reset the filling entirely
+
+    setFillingOfTheForm(structuredClone(formFeed));
+
+    return () => {
+      // clean up anything?
+    };
+  }, [formFeed]);
+
+
+  return (
+    <Ground>
+      <div
+        onClick={() => {
+          updateUrlQueryParams({
+            mode: 'merge',
+            newQuery: {
+              // aaaa778: String(Math.random()),
+              aaaa790: String(Math.random()),
+            },
+          });
+        }}
+      >
+        sfsdfsd_iIlL
+      </div>
+      <div>sfsdfsdsdfvsf</div>
+
+      <QuestionsList>
+        {formFeed.questions.map((question, questionIndex) => {
+          return (
+            <OneQuestion
+              key={question.id}
+              questionIndex={questionIndex}
+              question={question}
+            />
+          );
+        })}
+      </QuestionsList>
+    </Ground>
+  );
 };
--- src/feed/index.ts
+++ src/feed/index.ts
@@ -1,9 +1,9 @@
-import { n001_soulStyle_v01 } from '@/feed/n001_soulStyle/index_v01';
-import { n001_soulStyle_v02 } from '@/feed/n001_soulStyle/index_v02';
-import { n002_howInformedAboutEcology_v01 } from '@/feed/n002_howInformedAboutEcology/index_v01';
-import { n002_howInformedAboutEcology_v02 } from '@/feed/n002_howInformedAboutEcology/index_v02';
-import { n003_artTaste_v01 } from '@/feed/n003_artTaste/index_v01';
-import { n003_artTaste_v02 } from '@/feed/n003_artTaste/index_v02';
+import { n001_soulStyle_v01 } from '@/feed/domains/n001_soulStyle/versions/v01';
+import { n001_soulStyle_v02 } from '@/feed/domains/n001_soulStyle/versions/v02';
+import { n002_howInformedAboutEcology_v01 } from '@/feed/domains/n002_howInformedAboutEcology/versions/v01';
+import { n002_howInformedAboutEcology_v02 } from '@/feed/domains/n002_howInformedAboutEcology/versions/v02';
+import { n003_artTaste_v01 } from '@/feed/domains/n003_artTaste/versions/v01';
+import { n003_artTaste_v02 } from '@/feed/domains/n003_artTaste/versions/v02';
 
 /*
 
@@ -38,3 +38,60 @@ export const allForms = {
     v02: n003_artTaste_v02,
   },
 } as const;
+
+export type TyOneVersion = {
+  id: nsGlo.GoodId;
+  form: nsForm.One;
+};
+
+export type TyOneDomain = {
+  id: nsGlo.GoodId;
+  versions: TyOneVersion[];
+};
+
+export type TyAllForms = {
+  aaa: 'a';
+  domains: TyOneDomain[];
+};
+
+export const allForms741: TyAllForms = (() => {
+  const theThing: TyAllForms = {
+    aaa: 'a',
+    domains: [],
+  };
+
+  const allFormsToArr = Object.entries(allForms);
+
+  allFormsToArr
+    .sort((a, b) => {
+      // a[0] --> 'n001_soulStyle' --- slice(1, 4) --> '001'
+      const nOfA = Number(a[0].slice(1, 4));
+      const nOfB = Number(b[0].slice(1, 4));
+
+      if (nOfA === nOfB) {
+        throw new Error(`nOfA === nOfB --- sort --- allFormsToArr`);
+      }
+
+      return nOfA > nOfB ? 1 : -1;
+    })
+    .forEach((item) => {
+      // const currDomain: TyOneDomain = {
+      //   id: item[0],
+      // }
+      // theThing.domains.push();
+    });
+
+  console.log('allFormsToArr:', allFormsToArr);
+
+  return theThing;
+})();
+
+export const allFormsAsArr = (() => {
+  const arrOfDomains = Object.values(allForms);
+
+  const finalArr: nsForm.One[] = [];
+
+  arrOfDomains.forEach((domain) => {
+    // TODO:
+  });
+})();
